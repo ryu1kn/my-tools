519c95d44ec3e6db3078077bbdd6ad9ac676a43c
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-04-04 20:16:53 +1000

    Extracted the common logic of dotfile script

--- bin/link-dotfiles.sh
+++ bin/link-dotfiles.sh
@@ -3,21 +3,6 @@
 
 set -euo pipefail
 
-for ITEM in $REMOTE_CONFIG_DIR/_* ; do
-    CONFFILE=`basename $ITEM`
-    NODOTNAME=${CONFFILE#_}
+WALK_DOTFILES_SCRIPT=$(dirname "$0")/../lib/shell/walk-dotfiles.sh
 
-    SOURCE=$REMOTE_CONFIG_DIR/_$NODOTNAME
-    DEST=$HOME/.$NODOTNAME
-
-    if [[ -e $DEST ]] ; then
-        echo -n "Replace your local $DEST with $SOURCE ? (y/n): "
-        read REPLACE
-        [[ $REPLACE != Y ]] && [[ $REPLACE != y ]] && continue
-
-        mv -i $DEST $HOME/$NODOTNAME.bkp
-    elif [[ -L $DEST ]] ; then
-        rm -f $DEST
-    fi
-    ln -s $SOURCE $DEST
-done
+$WALK_DOTFILES_SCRIPT "ln -s"
---
+++ lib/shell/walk-dotfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Go through source dotfiles and execute the given command
+
+set -euo pipefail
+
+COMMAND=${1:-echo}
+
+for ITEM in $REMOTE_CONFIG_DIR/_* ; do
+    CONFFILE=`basename $ITEM`
+    NODOTNAME=${CONFFILE#_}
+
+    SOURCE=$REMOTE_CONFIG_DIR/_$NODOTNAME
+    DEST=$HOME/.$NODOTNAME
+
+    if [[ -e $DEST ]] ; then
+        echo -n "Replace your local $DEST with $SOURCE ? (y/n): "
+        read REPLACE
+        [[ $REPLACE != Y ]] && [[ $REPLACE != y ]] && continue
+
+        mv -i $DEST $HOME/$NODOTNAME.bkp
+    elif [[ -L $DEST ]] ; then
+        rm -f $DEST
+    fi
+
+    $COMMAND $SOURCE $DEST
+done
